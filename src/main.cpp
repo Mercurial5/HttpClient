2044f88095771b1c74913a8d83b8d1df7e933179
Mercurial <dias.nespayev@gmail.com>
2024-04-22 19:18:59 +0500

    chore: update an example

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 #include "client.h"
 #include "request.h"
+#include "response.h"
 #include "url.h"
-
 
 int main() {
     URL url("http://ip-api.com:80/json/");
     Request request(url);
 
     try {
-        std::stringstream response = Client::send(url);
-        std::cout << "Response: " << response.str() << std::endl;
+        std::stringstream response_ss = Client::send(url);
+        Response response(response_ss);
+
+        std::cout << "Status code: " << response.status_code << std::endl;
+        std::cout << "Headers: " << std::endl;
+        for (auto& i: response.headers) {
+            std::cout << '\t' << i.first << ": " << i.second << std::endl;
+        }
+        std::cout << std::endl << "Body: " << response.body << std::endl;
     } catch (Client::ClientError &e) {
         std::cout << e.what() << std::endl;
     }
